b1be166fcb9a15400a4aa6e200aa0314e13b82c1
N-Kazarian <nerses.kazarian@lacity.org>
2020-03-17 10:19:11 -0700

    Made Changes to ReasonCode.js

--- Business Rules Scripts/ReasonCode.js
+++ Business Rules Scripts/ReasonCode.js
@@ -1,13 +1,13 @@
 (function executeRule(current, previous /*null when async*/) {
 	
 	//Creates "SRType" String to place in ListOfLa311 array
-	var StringSR = String(current.srtype);
+	var StringSR = String(current.u_service_type);
 	
 	var words = StringSR.split(" -");
 	
 	var SRType = words[0];
 
-	var LAendpoint = "https://api-test.lacity.org/v2/myla311_qa/service-requests/" + current.srnumber;
+	var LAendpoint = "https://api-dev.lacity.org/v2/myla311_qa/bssproactive";
 	
 	//Creates RESTMessageV2
 	var request = new sn_ws.RESTMessageV2('x_428553_bss_sr.Upsert Comment', 'Default POST');
@@ -18,21 +18,28 @@
 	request.setRequestHeader("Content-Type", "Application/json");
 	
 	//Set Body
-	Body = ' {   "UpdatedByUserLogin": "STREETSLAINTEGRATION",' 
-		+  '  "ServiceDate": "",'
-		+  '  "ReasonCode": "' + current.reasoncode + '",'
-		+  '  "ListOfLa311 + ' + SRType +'": { '
-		+  '     "La311' + SRType + '": [ '
-        +  ' {'
-        +  '      "AssignedTo": '  + '"' + current.AssignedTo + '",'
-        +  '      "CompletedBy": ' + '"' + current.CompletedBy + '",'
-		+  '      "Crew": "' + current.crew + '", '
-		+  '      "Type": "' + SRType + '", '
-		+  '      "LastUpdatedBy": "' + current.LastUpdatedBy + '", '
-		+  '      "Name": "' + current.name + '" '
+	Body = ' {   "ServiceRequest": "STREETSLAINTEGRATION",' 
+		+  '  "AddressVerified": "P",'
+		+  '  "SRType": "' + current.u_service_type + '",'
+		+  '  "IntegrationID": "' + SRType + '",'
+		+  '  "CreatedByUserLogin": "STREETSLAINTEGRATION",'
+		+  '  "UpdatedUserLogin": "STREETSLAINTEGRATION",'
+		+  '  "Source": "Driver Self Report",'
+        +  '  "Owner": "BSS",'
+        +  '  "CreatedByUserOrganization": "Proactive Insert"'
+		+  '  "Status": "Open",'
+		+  '  "ReasonCode": "' + current.u_reason_code + '", '
+		+  '  "ServiceDate": "' + current.u_service_date + '", ' 
+		+  '  "SRAddress": "' + current.u_address + '", '
+		+  '  "ListOfLa311' + SRType +'": { '
+		+  '     "La311' + SRType + '": [{ '
+		+  '          "ApprovedBy": "' + current.u_approvedBy + '",'
+		+  '          "Crew": "' + current.u_crew + '",'
+		+  '          "Type": "' + SRType + '", '
+		+  '          "LastUpdatedBy": "' + current.u_last_updated_by + '", '
 		+  '}]'  
         +  '}'
-		+  '}';
+		+  '}}';
 	
 	request.setRequestBody(Body);
 	
