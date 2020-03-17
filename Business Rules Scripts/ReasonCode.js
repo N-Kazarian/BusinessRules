aa6f2fbbb2ec88cb79c444bd62655c6e1f196e5b
N-Kazarian <nerses.kazarian@lacity.org>
2020-03-17 13:26:25 -0700

    Made additional changes to ReasonCode.js

--- Business Rules Scripts/ReasonCode.js
+++ Business Rules Scripts/ReasonCode.js
@@ -1,16 +1,15 @@
-(function executeRule(current, previous /*null when async*/) {
-	
 	//Creates "SRType" String to place in ListOfLa311 array
-	var StringSR = current.u_service_type;
+
+	// var StringSR = current.u_service_type;
+
+	//var words = StringSR.split(" -");
 	
-	var words = StringSR.split(" -");
-	
-	var SRType = words[0];
+	// var SRType = words[0];
 
 	var LAendpoint = "https://api-dev.lacity.org/v2/myla311_qa/bssproactive";
 	
 	//Creates RESTMessageV2
-	var request = new sn_ws.RESTMessageV2('x_428553_bss_sr.Upsert Comment', 'Default POST');
+	var request = new sn_ws.RESTMessageV2('x_colac_streets_la.GetMyLA311Tickets', 'Default Post');
 
 	request.setEndpoint(LAEndpoint);
 	
@@ -18,37 +17,35 @@
 	request.setRequestHeader("Content-Type", "Application/json");
 	
 	//Set Body
-	Body = ' {   "ServiceRequest": {,' 
+	var Body = ' {   "ServiceRequest": {' 
 		+  '  "AddressVerified": "P",'
-		+  '  "SRType": "' + current.u_service_type + '",'
-		+  '  "IntegrationID": "' + SRType + '",'
+		+  '  "SRType": "Weed Abatement",'
+		+  '  "IntegrationID": "BSS-APro2",'
 		+  '  "CreatedByUserLogin": "STREETSLAINTEGRATION",'
 		+  '  "UpdatedUserLogin": "STREETSLAINTEGRATION",'
 		+  '  "Source": "Driver Self Report",'
-        +  '  "Owner": "BSS",'
+      	+  '  "Owner": "BSS",'
         +  '  "CreatedByUserOrganization": "Proactive Insert,"'
 		+  '  "Status": "Open",'
-		+  '  "ReasonCode": "' + current.u_reason_code + '", '
-		+  '  "ServiceDate": "' + current.u_service_date + '", ' 
-		+  '  "SRAddress": "' + current.u_address + '", '
-		+  '  "ListOfLa311' + SRType +'": { '
-		+  '     "La311' + SRType + '": [{ '
-		+  '          "ApprovedBy": "' + current.u_approvedBy + '",'
-		+  '          "AssignedTo": "' + current.u_assigned_to + '",'
+		+  '  "ReasonCode": "NF", '
+		+  '  "ServiceDate": "01/08/2020 22:43:00", ' 
+		+  '  "SRAddress": "123 Test Street, 91402", '
+		+  '  "ListOfLa311NonCompliantVending": { '
+		+  '     "La311NonCompliantVending": [{ '
+		+  '          "ApprovedBy": "Ross",'
+		+  '          "AssignedTo": "Nerses",'
 		+  '          "Contact": "", '
 		+  '          "ContactDate": "01/08/2020 22:43:00",'
-		+  '          "Crew": "' + current.u_crew + '",'
+		+  '          "Crew": "Blue",'
 		+  '          "InspectedBy": "Narek",'
 	    +  '          "InspectionDate": "01/08/2020 22:43:00",'
 		+  '          "CompletedBy": "Ross",'
-		+  '          "Type": "' + SRType + '", '
-		+  '          "LastUpdatedBy": "' + current.u_last_updated_by + '" '
+		+  '          "Type": "Non-Compliant Vending", '
+		+  '          "LastUpdatedBy": "STREETSLAINTEGRATION" '
 		+  '}]'  
-        +  '}'
+                +  '}'
 		+  '}}';
 	
 	request.setRequestBody(Body);
 	
 	request.execute();
-
-})(current, previous);
