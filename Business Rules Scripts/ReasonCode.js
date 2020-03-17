327ffe73797e3bac2793e21bbdca74ab820fa12c
N-Kazarian <nerses.kazarian@lacity.org>
2020-03-17 14:01:25 -0700

    Additional Changes to ReasonCode.js

--- Business Rules Scripts/ReasonCode.js
+++ Business Rules Scripts/ReasonCode.js
@@ -1,8 +1,8 @@
-	//Creates "SRType" String to place in ListOfLa311 array
+	// Creates "SRType" String to place in ListOfLa311 array
 
 	// var StringSR = current.u_service_type;
 
-	//var words = StringSR.split(" -");
+	// var words = StringSR.split(" -");
 	
 	// var SRType = words[0];
 
@@ -12,41 +12,32 @@
 	var request = new sn_ws.RESTMessageV2('x_colac_streets_la.GetMyLA311Tickets', 'Default Post');
 
 	request.setEndpoint(LAEndpoint);
-	
-	//Set Headers
-	request.setRequestHeader("Content-Type", "Application/json");
-	
+
 	//Set Body
 	var Body = ' {   "ServiceRequest": {' 
 		+  '  "AddressVerified": "P",'
-		+  '  "SRType": "Weed Abatement",'
-		+  '  "IntegrationID": "BSS-APro2",'
+		+  '  "SRType": "Weed Abatement for Pvt Parcelst",'
+		+  '  "IntegrationId": "BSS-APro2",'
 		+  '  "CreatedByUserLogin": "STREETSLAINTEGRATION",'
-		+  '  "UpdatedUserLogin": "STREETSLAINTEGRATION",'
+		+  '  "UpdatedByUserLogin": "STREETSLAINTEGRATION",'
 		+  '  "Source": "Driver Self Report",'
       	+  '  "Owner": "BSS",'
-        +  '  "CreatedByUserOrganization": "Proactive Insert,"'
-      	+  '  "Owner": "BSS",'
-        +  '  "CreatedByUserOrganization": "Proactive Insert,"'
+        +  '  "CreatedByUserOrganization": "Proactive Insert",'
 		+  '  "Status": "Open",'
 		+  '  "ReasonCode": "NF", '
 		+  '  "ServiceDate": "01/08/2020 22:43:00", ' 
 		+  '  "SRAddress": "123 Test Street, 91402", '
-		+  '  "ListOfLa311NonCompliantVending": { '
-		+  '     "La311NonCompliantVending": [{ '
-		+  '          "ApprovedBy": "Ross",'
+		+  '  "ListOfLa311WeedAbatementForPrivateParcels": { '
+		+  '     "La311WeedAbatementForPrivateParcels": [{ '
 		+  '          "AssignedTo": "Nerses",'
-		+  '          "Contact": "", '
+		+  '          "Contact": "",'
 		+  '          "ContactDate": "01/08/2020 22:43:00",'
 		+  '          "Crew": "Blue",'
 		+  '          "InspectedBy": "Narek",'
 	    +  '          "InspectionDate": "01/08/2020 22:43:00",'
-		+  '          "CompletedBy": "Ross",'
-		+  '          "Type": "Non-Compliant Vending", '
+		+  '          "Type": "Weed Abatement",'
 		+  '          "LastUpdatedBy": "STREETSLAINTEGRATION" '
-		+  '}]'  
-                +  '}'
-		+  '}}';
+		+  '}]}}}';
 	
 	request.setRequestBody(Body);
 	
